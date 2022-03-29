73afa4d601cb1408664581ab4d7cb21da607fa7f
Lukáš Chylík <lukas.chylik@riganti.cz>
2022-03-29 11:22:10 +0200

    email changes

--- Resources/Scripts/init.ts
+++ Resources/Scripts/init.ts
@@ -1,11 +1,13 @@
-﻿import { initNav } from "./navigation";
-import { initMap } from "./map";
-import { initAnim } from "./boxAnim";
-import { setCurentYear } from "./year";
+﻿import { initNav } from "./navigation"
+import { initMap } from "./map"
+import { initAnim } from "./boxAnim"
+import { setCurentYear } from "./year"
+import { initEmail } from "./email"
 
 document.addEventListener("DOMContentLoaded", function () {
-  initNav();
-  initAnim();
-  initMap();
-  setCurentYear();
-});
+  initNav()
+  initAnim()
+  initMap()
+  setCurentYear()
+  initEmail()
+})
